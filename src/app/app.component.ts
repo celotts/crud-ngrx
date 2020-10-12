8e96e9484e7b20d06dca63cababfd04a2a8324a9
= <celotts@gmail.com>
2020-10-12 20:17:00 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import * as fromStore from './store';
 import { ICustomer } from './models/customer.model';
 import { CustomerService } from './services/customer.service';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,10 +12,11 @@ import { CustomerService } from './services/customer.service';
 export class AppComponent {
   customers: ICustomer[];
 
-  // tslint:disable-next-line: use-lifecycle-interface
-  ngOnInit(): void {
-    this.store.dispatch(new fromStore.LoadCustomer());
-  }
+  // tslint:disable-next-line: ban-types
+  display: String = 'none';
+  isEditModeEnabled = false;
+  person: ICustomer = {};
+
   constructor(private store: Store<fromStore.IAppState>, custumerService: CustomerService) {
     store.select(fromStore.getCustomers).subscribe(resCustomer => {
       this.customers = resCustomer;
@@ -23,6 +25,39 @@ export class AppComponent {
     store.select(fromStore.getCustumerById(2)).subscribe(resCustomer => {
       console.log(resCustomer);
     });
+
+  }
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit(): void {
+    this.store.dispatch(new fromStore.LoadCustomer());
+  }
+
+
+  updateCustomer(myForm) {
+
+  }
+
+  closeModal(myForm: NgForm) {
+    this.display = 'none';
+  }
+
+  addCustomer(myForm) {
+
+  }
+
+  openModal() {
+    this.display = 'block';
+  }
+
+  edit(item) {
+
+    this.isEditModeEnabled = true;
+    // tslint:disable-next-line: no-unused-expression
+    this.person = { ...item };
+    this.display = 'block';
+  }
+
+  remove(item) {
 
   }
 }
